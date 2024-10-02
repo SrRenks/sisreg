9d9f3457440d5058ae68e89491d2e1337357d704
Renks <renks@localhost.localdomain>
2024-10-02 15:48:16 -0300

    change: adapted method for new relatory return

--- src/main/main.py
+++ src/main/main.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import pandas as pd
 import argparse
 import tempfile
+import locale
 import psutil
 import json
 import sys
@@ -15,6 +16,7 @@ import re
 if __name__ == '__main__':
     from_date = datetime.today().strftime("%d/%m/%Y")
     to_date = (datetime.today() + timedelta(7)).strftime("%d/%m/%Y")
+    locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
     parser = argparse.ArgumentParser(description='Sisreg class arg parser')
     parser.add_argument('--username', '-u', type=str, help='Login Username', required=True)
     parser.add_argument('--password', '-p', type=str, help='Login Password', required=True)
@@ -73,15 +75,12 @@ if __name__ == '__main__':
 
                 pbar.update(1), pbar.set_postfix(unit=f"{worker_param['unit'][:20]}{'+' if len(worker_param['unit']) > 20 else ''}")
 
-    with open(os.path.join("resources", "relatory_flags.json"), "r") as file:
-        flags = json.loads(file.read())
-
     with tqdm(total=len(units), ascii=' ━', colour='GREEN', dynamic_ncols=True, unit="data",
               desc=f"get method data (threads: {threads})", postfix={"unit": ""}, leave=False) as pbar:
 
         unit_futures_map = {}
         with concurrent.futures.ThreadPoolExecutor(threads) as executor:
-            futures = {executor.submit(sisreg.get_worker_schedule_relatory, method, **flags): method for method in units}
+            futures = {executor.submit(sisreg.get_worker_schedule_relatory, method): method for method in units}
             units.clear()
             unit_futures_map.update(futures)
             for future in concurrent.futures.as_completed(futures):
@@ -93,20 +92,12 @@ if __name__ == '__main__':
     with open(os.path.join("resources", "unit_address.json"), "r") as file:
         addresses = json.loads(file.read())
 
-    def fix_wrong_lines(row: pd.Series, wrong_lines: pd.DataFrame, to_fix: pd.DataFrame) -> pd.Series:
-        if row.name in wrong_lines.index:
-            previous_index = to_fix[to_fix.index < row.name].index.max()
-            method = next(value for value in row if isinstance(value, str) and re.search(r'^(\d+) - ', value))
-            row[:] = df.loc[previous_index]
-            row[["Procedimento", "Procedimento.1"]] = method
-        return row
-
     df = pd.DataFrame(units, dtype=str)
-    wrong_lines = df[df.apply(lambda row: any(pd.to_numeric(row.str.extract(r'^(\d+)\s+\-\s+', expand=False).squeeze()) > 1), axis=1)]
-    to_fix = df[df.index.isin(wrong_lines.index - 1) & df.apply(lambda row: row.str.contains(r'^01\s+\-\s+').any(), axis=1)]
-    df = df.apply(fix_wrong_lines, wrong_lines=wrong_lines, to_fix=to_fix, axis=1)
-    df['Data/Hora'] = df['Data/Hora'].apply(lambda x: datetime.strptime(re.search(r"(\d{1,2}\/\d{1,2}\/\d{4}\s+\d{2}\:\d{2})", x).group(1), "%d/%m/%Y %H:%M") if pd.notnull(x) else None)
+    df['Data/Hora'] = df['Data/Hora'].apply(lambda x: (datetime.strptime(f"{match.group(1)} {match.group(2)}", "%d/%m/%Y %H:%M")
+                                            if pd.notnull(x) and (match := re.search(r"(\d{1,2}/\d{1,2}/\d{4})\s+-\s+\w{3}\s+-\s+(\d{2}:\d{2})", x)) else None))
+
     df[['Data', 'Hora']] = df['Data/Hora'].apply(lambda x: pd.Series([x.strftime("%d/%m/%Y"), x.strftime("%H:%M")]) if pd.notnull(x) else pd.Series([None, None]))
+    df['Procedimento(s)'] = df['Procedimento(s)'].apply(lambda x: re.sub(r'\s*\(\d{10}\)$', '', x) if pd.notnull(x) else x)
     df['Endereco'] = df['Unidade'].apply(lambda x: addresses.get(x, ""))
     df = df.map(lambda x: re.sub(r"^\d+\s+\-\s+", '', x) if isinstance(x, str) else x)
     df.replace("---", None, inplace=True)
@@ -128,7 +119,7 @@ if __name__ == '__main__':
         if os.path.exists(banlist[0]):
             banlist = open(banlist[0], "r").read().splitlines()
 
-        df = df[~df['Procedimento'].str.contains(re.compile(r'|'.join(value for value in banlist)), na=False)]
+        df = df[~df['Procedimento(s)'].str.contains(re.compile(r'|'.join(value for value in banlist)), na=False)]
 
     if args["export_type"] == "xlsx":
         with tempfile.NamedTemporaryFile(suffix='.xlsx', delete=False) as temp_file:
